bb321995b613ed12253197bc5bbd78c19e02cb77
Jimil Soni <jimilsoni926@gmail.com>
2025-08-02 14:51:47 +0530

    Setup vite and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-300'>
+      <h1>Welcome to Mini LinkedIn!</h1>
+      <p>This is a sample React app using Vite.</p>
+    </div>
+  )
+}
+
+export default App
